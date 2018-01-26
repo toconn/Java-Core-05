1e15d3b05c454a9dca49d1acacf5233e445ae65a
Tim OConnell <tuc.github@uaconaill.com>
2018-01-25 22:22:39 -0500

    Added equals, hashCode to CaseString.

--- Source/Main/Java/ua/core/data/CaseString.java
+++ Source/Main/Java/ua/core/data/CaseString.java
@@ -93,4 +93,34 @@ public class CaseString {
             "value=" + ((value != null) ? "\"" + value + "\"" : "null") +
             "]";
     }
+    
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// Do Not Overwrite With Generated Code...                                                                       //
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CaseString other = (CaseString) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+    
 }
